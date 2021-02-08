6843c0c662f62eb0c92695864f0f5408b96711db
JP Castrillon <jpcas001@gmail.com>
2021-02-08 12:25:04 -0500

    updating main components

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://i.imgur.com/INl1ntD.png">
+    <HelloWorld msg="Welcome to Gracianos Bike Shop"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
